e964e401ee8b430199387fe85e88c1c32a6c4d90
Vladislav Zenkevic <vladislav.zenkevic@gmail.com>
2019-10-14 15:55:13 +0200

    Task Deux done

---
+++ FinalGame/Assets/Game.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private List <IGameLoop> GameLoops = new List<IGameLoop>();
+    private bool inErrorState;
+
+public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        try
+        {
+            if (!inErrorState)
+                foreach (var gameLoop in GameLoops)
+                    gameLoop.Update();
+        }
+        catch (System.Exception e)
+        {
+            HandleGameLoopException(e);
+            throw;
+        }
+    }
+
+    void HandleGameLoopException(System.Exception e)
+    {
+        Debug.Log("EXCEPTION: " + e.Message + "\n" + e.StackTrace);
+        // Time.timeScale = 0; // If certain game objects continue some behaviour, uncomment this line.
+        inErrorState = true;
+    }
+
+    public void AddGameLoop(IGameLoop gameLoop) { GameLoops.Add(gameLoop); }
+    public interface IGameLoop
+    {
+        void Update();
+        // void FixedUpdate();
+        // void LateUpdate();
+    }
+}
