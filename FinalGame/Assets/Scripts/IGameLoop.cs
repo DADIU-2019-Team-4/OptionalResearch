5d140ab40c196b23437f674b5b807c494e370a9e
Vladislav Zenkevic <vladislav.zenkevic@gmail.com>
2019-10-17 12:50:29 +0200

    Task Trois done
    Task done with having IGameLoop classes instead of having abstract interface. Reference to solution - https://blogs.unity3d.com/2015/12/23/1k-update-calls/

---
+++ FinalGame/Assets/Scripts/IGameLoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IGameLoop : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _sceneTwo;
+    public string objectName;
+    private void OnEnable()
+    {
+        var game = FindObjectOfType<Game>();
+        game.instance.AddGameLoop(this);
+    }
+    public void CustomUpdate()
+    {
+        _sceneTwo = GameObject.Find(objectName);
+    }
+}
+    
